6cb14bbde2a7739b6c06ff04b6047e56c233092b
Jobsz <brucezcq@gmail.com>
2019-12-19 22:43:25 +0800

    using SpringRunner

--- src/test/java/cn/zhucongqi/JacksonTest.java
+++ src/test/java/cn/zhucongqi/JacksonTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author ：Jobsz
@@ -16,7 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @modified By：
  * @version:
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class)
 public class JacksonTest {
 
